6bf2f8db1dfd765c5516ca95f5668e3f78696b23
Silvina <scorteslasalle@gmail.com>
2022-04-04 19:04:18 -0300

    subir archivos

---
+++ Obligatorio1/Obligatorio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Obligatorio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
